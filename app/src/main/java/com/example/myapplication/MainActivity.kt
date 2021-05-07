d0fe8743b1f7be16beca9ee732d236a2becac3c9
sunjichao <mm18561784507@163.com>
2021-05-07 14:23:36 +0800

    添加sex

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     var count:Int=0
+    var sex:String="男"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
